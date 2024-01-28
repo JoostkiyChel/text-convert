f2cf1e0c284b1357facb5085ddda5fe24cf2500b
JoostkiyChel <tilox228133786@gmail.com>
2024-01-29 00:04:53 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PW6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Run start = new Run();
+
+            start.MainMenu();
+        }
+    }
+}
---
+++ ConsoleApp4/model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{    
+        public class Model
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Description { get; set; }
+
+            public Model(string name, int age, string description)
+            {
+                Name = name;
+                Age = age;
+                Description = description;
+            }
+        }    
+}
